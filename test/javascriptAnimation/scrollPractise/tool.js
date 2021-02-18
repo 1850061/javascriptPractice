7639b65204879a903cf1487508fcdc50111425ed
1850061 <1763244792@qq.com>
2021-02-18 23:18:31 +0800

    add a function to get client's width and height

--- test/javascriptAnimation/scrollPractise/tool.js
+++
@@ -1,6 +0,0 @@
-function scroll() {
-    return {
-        top:window.pageYOffset||document.body.scrollTop||document.documentElement.scrollTop,
-        left:window.pageXOffset||document.body.scrollLeft||document.documentElement.scrollLeft
-    }
-}
---
+++ test/javascriptAnimation/tool.js
@@ -0,0 +1,28 @@
+function scroll() {
+    return {
+        top:window.pageYOffset||document.body.scrollTop||document.documentElement.scrollTop,
+        left:window.pageXOffset||document.body.scrollLeft||document.documentElement.scrollLeft
+    }
+}
+
+function client() {
+    if (window.innerHeight !== undefined) {
+        //ie9及其以上的版本的写法
+        return {
+            "width": window.innerWidth,
+            "height": window.innerHeight
+        }
+    } else if (document.compatMode === "CSS1Compat") {
+        //标准模式的写法（有DTD时）
+        return {
+            "width": document.documentElement.clientWidth,
+            "height": document.documentElement.clientHeight
+        }
+    } else {
+        //没有DTD时的写法
+        return {
+            "width": document.body.clientWidth,
+            "height": document.body.clientHeight
+        }
+    }
+}
